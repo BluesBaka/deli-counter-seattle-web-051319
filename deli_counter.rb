3a0a148a193c06c8aaaa6929d00dc335cc417034
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 06:52:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,6 @@ end
 def take_a_number(katz_deli, name)
     katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
-    katz_deli.push(name)
 end
 
 def now_serving(katz_deli)
