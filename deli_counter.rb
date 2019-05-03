66f5a62e1976b7d6682a44b6a48e2e910f31e842
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 06:18:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
+deliLine =[]
+def line(name)
+  if deliLine = 0
+    return "The line is empty."
+  end
+  puts "The line is currently #{deliLine}"
+  
+end
+
+def take_a_number(name)
+
+end
+
+def now_serving
+    if deliLine = 0
+      return "The line is empty."
+    end
+end
 # Write your code here.
