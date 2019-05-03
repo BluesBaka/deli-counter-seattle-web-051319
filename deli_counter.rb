3ad478878923652b28dc3d2337f6f76ef7093fd7
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 06:28:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
 
-  puts "The line is currently: #{katz_deli}"
+
+  puts "The line is currently: #{katz_deli.each_with_index}"
 
 end
 
