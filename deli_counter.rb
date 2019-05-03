b3232d997f943b0c2c66a871801f3737eb589252
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 06:23:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(name)
   if katz_deli = 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   end
   puts "The line is currently: #{katz_deli}"
 
