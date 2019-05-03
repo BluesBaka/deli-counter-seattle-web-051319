2b1bd1be2c14218db050cff260d9a93572b966b5
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 07:01:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,6 @@
 def line(katz_deli)
   if katz_deli == 0
     puts "The line is currently empty."
-  else
-    puts "The line is currently: #{katz_deli}"
   end
   puts "The line is currently: #{katz_deli.each_with_index(1)}"
 
